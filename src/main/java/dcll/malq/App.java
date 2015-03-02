d463ac55b0205d214f92bdafdbea7810bc695a49
Marcfr <marcalquier81@gmail.com>
2015-03-02 10:52:54 +0100

    TP2 exo2.3

---
+++ src/main/java/dcll/malq/App.java
@@ -0,0 +1,13 @@
+package dcll.malq;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
